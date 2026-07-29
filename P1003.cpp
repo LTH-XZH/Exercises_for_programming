95b55a6eb830fbb5bd72281d3dd370416cf3434c
LTH-XZH <2192894594@qq.com>
2026-07-29 18:49:57 +0800

    P1003

--- P1003.cpp
+++ P1003.cpp
@@ -6,16 +6,16 @@ struct carpet
 } carpet_use[10000];
 int main()
 {
-    int n,x,y;
+    int n, x, y;
     cin >> n;
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         cin >> carpet_use[i].a >> carpet_use[i].b >> carpet_use[i].g >> carpet_use[i].k;
     }
     cin >> x >> y;
-    for(int i = n-1; i >= 0; i--)
+    for (int i = n - 1; i >= 0; i--)
     {
-        if(x >= carpet_use[i].a && x <= carpet_use[i].a + carpet_use[i].g && y >= carpet_use[i].b && y <= carpet_use[i].b + carpet_use[i].k)
+        if (x >= carpet_use[i].a && x <= carpet_use[i].a + carpet_use[i].g && y >= carpet_use[i].b && y <= carpet_use[i].b + carpet_use[i].k)
         {
             cout << i + 1 << endl;
             return 0;
