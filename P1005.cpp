c54b7dda29f000ccb12439278888c4ab4d66216a
LTH-XZH <2192894594@qq.com>
2026-08-11 21:38:34 +0800

    通过60%数据，因long long数据会溢出

--- P1005.cpp
+++ P1005.cpp
@@ -14,24 +14,26 @@ int main()
             cin >> matrix[i][j];
         }
     }
-    long long max_sum = 0;
-    for (int i = 1; i <= n; i++)
+    unsigned long long ans = 0;
+    for (int p = 1; p <= n; p++)
     {
-        int lt = 1, rt = m;
-        for (int j = 1; j <= m; j++)
+        unsigned long long max_sum = 0;
+        unsigned long long max_for_line[85][85] = {0};
+        max_for_line[1][m] = 0;
+        for(int len = m; len >= 1; len--)
         {
-            if (matrix[i][lt] < matrix[i][rt])
+            for(int i = 1; i <= m - len + 1; i++)
             {
-                max_sum += matrix[i][lt] * pow(2, j);
-                lt++;
-            }
-            else
-            {
-                max_sum += matrix[i][rt] * pow(2, j);
-                rt--;
+                int j = i + len - 1;
+                max_for_line[i][j] = max((max_for_line[i - 1][j] + matrix[p][i - 1] * pow(2, m - j + i - 1)) * (i - 1 >= 0), (max_for_line[i][j + 1] + matrix[p][j + 1] * pow(2, m - j + i - 1)) * (j + 1 <= m));
             }
         }
+        for (int i = 1; i <= m; i++)
+        {
+            max_sum = max(max_sum, max_for_line[i][i] + matrix[p][i] * (unsigned long long)pow(2, m));
+        }
+        ans += max_sum;
     }
-    cout << max_sum << endl;
+    cout << ans << endl;
     return 0;
 }
