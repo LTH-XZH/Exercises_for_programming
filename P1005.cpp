451ffb18e7883507a140881356eb9fd97a1420ed
LTH-XZH <2192894594@qq.com>
2026-08-11 21:41:07 +0800

    定义高精度类并重新定义运算，添加了“high_num.h”文件

--- P1005.cpp
+++ P1005.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <cmath>
+#include "high_num.h"
+
 using namespace std;
+
+high_num max_for_line[85][85];
 
 int main()
 {
     int n, m;
-    long long matrix[85][85];
+    int matrix[85][85];
     cin >> n >> m;
     for (int i = 1; i <= n; i++)
     {
@@ -14,26 +18,39 @@ int main()
             cin >> matrix[i][j];
         }
     }
-    unsigned long long ans = 0;
+    high_num ans;
     for (int p = 1; p <= n; p++)
     {
-        unsigned long long max_sum = 0;
-        unsigned long long max_for_line[85][85] = {0};
+        high_num max_sum;
         max_for_line[1][m] = 0;
-        for(int len = m; len >= 1; len--)
+        for (int len = m; len >= 1; len--)
         {
-            for(int i = 1; i <= m - len + 1; i++)
+            for (int i = 1; i <= m - len + 1; i++)
             {
                 int j = i + len - 1;
-                max_for_line[i][j] = max((max_for_line[i - 1][j] + matrix[p][i - 1] * pow(2, m - j + i - 1)) * (i - 1 >= 0), (max_for_line[i][j + 1] + matrix[p][j + 1] * pow(2, m - j + i - 1)) * (j + 1 <= m));
+                if(i-1>=1 && j+1<=m)
+                    max_for_line[i][j] = high_num::max_high((max_for_line[i - 1][j] + high_num::power_2(m - j + i - 1) * matrix[p][i - 1]), (max_for_line[i][j + 1] + high_num::power_2(m - j + i - 1) * matrix[p][j + 1]));
+                else if(i-1>=1)
+                    max_for_line[i][j] = (max_for_line[i - 1][j] + high_num::power_2(m - j + i - 1) * matrix[p][i - 1]);
+                else if(j+1<=m)
+                    max_for_line[i][j] = (max_for_line[i][j + 1] + high_num::power_2(m - j + i - 1) * matrix[p][j + 1]);
+                else
+                    max_for_line[i][j] = 0;
             }
         }
         for (int i = 1; i <= m; i++)
         {
-            max_sum = max(max_sum, max_for_line[i][i] + matrix[p][i] * (unsigned long long)pow(2, m));
+            max_sum = high_num::max_high(max_sum, max_for_line[i][i] + high_num::power_2(m) * matrix[p][i]);
         }
-        ans += max_sum;
+        ans = ans + max_sum;
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                max_for_line[i][j] = 0;
+            }
+        }
     }
-    cout << ans << endl;
+    ans.print();
     return 0;
 }
